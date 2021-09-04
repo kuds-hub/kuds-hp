682096ca387771fa62465e69a07f4334e20c3d11
s11142 <shotanagaizumi@gmail.com>
2021-09-04 17:18:20 +0900

    init commit backend

---
+++ homepage/src/test/java/jp/co/kuds/homepage/HomepageApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.kuds.homepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomepageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
